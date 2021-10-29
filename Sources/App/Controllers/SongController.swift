d4d20482435b4f68f264190b57c4e8133ca9b963
Andrei Chenchik <andrei@chenchik.me>
2021-10-29 15:36:17 +0200

    Hide all additional logic, expose 80 port

--- Sources/App/Controllers/SongController.swift
+++ Sources/App/Controllers/SongController.swift
@@ -5,12 +5,12 @@
 //  Created by Andrei Chenchik on 28/10/21.
 //
 
-import Fluent
-import Vapor
-
-struct SongController: RouteCollection {
-    func boot(routes: RoutesBuilder) throws {
-        let songs = routes.grouped("songs")
-        
-    }
-}
+//import Fluent
+//import Vapor
+//
+//struct SongController: RouteCollection {
+//    func boot(routes: RoutesBuilder) throws {
+//        let songs = routes.grouped("songs")
+//
+//    }
+//}
